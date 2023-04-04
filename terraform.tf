d94c6387fd6c73566e3b1e9245969d061a6e2fd5
Ubuntu <alfjury@gmail.com>
2023-04-04 20:22:42 -0300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "alfredojury-org1"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
